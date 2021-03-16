89c261d248f84ce09b797cc4f17c985f857b2800
oscar <oscarbarragan31@gmail.com>
2021-03-16 09:00:45 +0000

    01 cre la clase test y escribi el codigo de la clase normal para que diera fallo

---
+++ ComprobadorDeMultiplos.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class ComprobadorDeMultiplos here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ComprobadorDeMultiplos
+{
+    public boolean esMultiploDe2(int numero){
+        return true;
+    }
+    
+    public boolean esMultiploDe5(int numero){
+        return false;
+    }
+     
+    public boolean esMultiploDe(int a, int b){
+       return true;
+    }
+}
+
