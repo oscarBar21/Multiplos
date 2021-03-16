420d7f5612dba4779dcd24a01f7fd07717968866
oscar <oscarbarragan31@gmail.com>
2021-03-16 09:36:16 +0000

    01 modifique la clase normal para que ahora pase los test correctamente

--- ComprobadorDeMultiplos.java
+++ ComprobadorDeMultiplos.java
@@ -8,15 +8,26 @@
 public class ComprobadorDeMultiplos
 {
     public boolean esMultiploDe2(int numero){
-        return true;
+        boolean resultado = false;
+        if((numero % 2 == 0) && (numero >= 2)){
+            resultado = true;
+        }
+        return resultado;
     }
     
     public boolean esMultiploDe5(int numero){
-        return false;
+        boolean resultado = false;
+        if((numero % 5 == 0) && (numero >= 5)){
+            resultado = true;
+        }
+        return resultado;
     }
-     
+    
     public boolean esMultiploDe(int a, int b){
-       return true;
+        boolean resultado = false;
+        if((b != 0) && (a >= b) && (b > 0) && (a >= 0) && (a % b == 0 )){
+            resultado = true;
+        }
+        return resultado;
     }
 }
-
